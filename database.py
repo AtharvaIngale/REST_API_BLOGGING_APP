d5699191c28b1b7b3690fe5737c797e0c1be8d1b
Atharva Ingale <94461630+AtharvaIngale@users.noreply.github.com>
2024-05-06 17:26:43 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client["blog_db"]  # Replace with your database name
+db = client["blog_db"]  #database name
 
 async def get_blog_posts():
     posts = db.posts.find()
